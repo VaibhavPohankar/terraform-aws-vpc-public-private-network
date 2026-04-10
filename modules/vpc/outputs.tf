46d79def41db5e7183bfcc67dc762de288a99e7c
vaibhavpohankar <vaibhavpohankar30@gmail.com>
2026-04-10 16:13:47 +0530

    Add NAT gateway outputs to VPC module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,15 @@
 output "vpc_id" {
   value = aws_vpc.this.id
 }
-
 output "public_subnets" {
   value = [for s in aws_subnet.public : s.id]
 }
-
 output "private_subnets" {
   value = [for s in aws_subnet.private : s.id]
+}
+output "nat_gateway_id" {
+  value = length(aws_nat_gateway.nat) > 0 ? aws_nat_gateway.nat[0].id : null
+}
+output "nat_public_ip" {
+  value = length(aws_eip.nat) > 0 ? aws_eip.nat[0].public_ip : null
 }
